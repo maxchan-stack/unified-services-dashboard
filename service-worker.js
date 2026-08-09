d8e6d9d6c8e7af8329aee1570f043da1ec506f2c
maxchan-stack <maxgdodo@gmail.com>
2026-08-09 21:09:04 +0800

    fix: 更新雲端試算表捷徑選單為真實 Google 試算表編輯網址 (v52)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'omnihub-shell-v51';
+const CACHE_NAME = 'omnihub-shell-v52';
 const STATIC_ASSETS = [
   './',
   './index.html',
@@ -38,7 +38,7 @@ self.addEventListener('install', (event) => {
   self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('[ServiceWorker] Pre-caching App Shell v51');
+      console.log('[ServiceWorker] Pre-caching App Shell v52');
       return cache.addAll(STATIC_ASSETS);
     })
   );
